02a6525cda3b6cfe5fb8b6b1dcabb43f63e74993
NAJIB-B <najibajibasu@gmail.com>
2024-09-04 14:44:26 +0100

    Implemented the home page for natours, 30% done with the implementation of the tour page

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const path = require('path')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes')
+const viewRouter = require('./routes/viewRoutes')
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -68,25 +69,8 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The forest Hiker',
-    user: 'Jonas'
-  })
-})
 
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  })
-})
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The forest hiker'
-  })
-})
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ controllers/viewController.js
@@ -0,0 +1,31 @@
+const Tour = require('../models/tourModels')
+const catchAsync = require('../utils/catchAsync')
+
+
+
+exports.getOverview =catchAsync(async (req, res, next) => {
+  // 1 get tour data from collection
+  const tours = await Tour.find();
+
+  
+  // 2 build templates
+  // render template using tour data
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
+  })
+})
+
+exports.getTour = catchAsync(async (req, res, next) => {
+	const {slug} = req.params
+	let query = Tour.findOne({slug})
+
+	query = query.populate({path: 'reviews', fields: 'review rating user'})
+	const tour = await query
+	
+
+  res.status(200).render('tour', {
+    title: 'The forest hiker',
+	tour
+  })
+})
